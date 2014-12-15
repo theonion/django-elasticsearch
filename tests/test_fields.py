68f62f5eed73db69480dfe54f5b3b4a9004d5f03
vforgione <the.vincedent@icloud.com>
2014-12-15 15:41:06 -0600

    working on testing

---
+++ tests/test_fields.py
@@ -0,0 +1,12 @@
+import unittest
+
+from djelastic import fields
+
+
+class FieldsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        super(FieldsTestCase, self).setUp()
+
+    def test__define_mapping(self):
+        pass
